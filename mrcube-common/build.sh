4a241f951bf46644f744cbeb16693a7dae3bc20a
Takeshi Morita <morita@it.aoyama.ac.jp>
2026-04-23 23:24:11 +0900

    update build.sh and Dockerfile

--- mrcube-common/build.sh
+++ mrcube-common/build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-APP_VERSION=$(cat ../VERSION)
-docker build --no-cache=true --platform linux/amd64 -t morita/mrcube:${APP_VERSION}-common .
+docker build --no-cache=true --platform linux/amd64 -t morita/mrcube:common .
